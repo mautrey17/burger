11371026be36c2ba37559f24b1fcb985d696c75c
Matt Autrey <mautrey17@gmail.com>
2021-03-06 10:02:10 -0500

    select all function in orm

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,23 @@
 //made from scratch ORM
-
+//dependenncies
 const connection = require('./connection.js');
+
+const orm = {
+    selectAll(table, callback) {
+        const queryString = `select * from ${table};`
+        connection.query(queryString, (err, burgerInfo) => {
+            if(err){throw err};
+            callback(burgerInfo);
+        })
+    },
+
+    insertOne(table, cols, values, cb) {
+        const queryString = `insert into ${table}`
+    },
+
+    updateOne() {
+
+    }
+}
+
+module.exports = orm;
