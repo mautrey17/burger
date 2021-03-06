3fb2a6d2eec4f1931019eabdd71bff49dec356d6
Matt Autrey <mautrey17@gmail.com>
2021-03-06 08:55:48 -0500

    connection file

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,24 @@
-//connection to MySQL
+//establish connection with MySQL
+
+//dependencies
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '1234',
+  database: 'burgers_db',
+});
+
+// establish connection
+connection.connect((err) => {
+  if(err){
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+//export connection
+module.exports = connection;
