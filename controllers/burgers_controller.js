ba21042fd2bc65e20543af7a8cb616855a3e70b4
Matt Autrey <mautrey17@gmail.com>
2021-03-07 16:37:19 -0500

    routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,19 @@
 const burger = require('../')
 const express = require('express');
 
+const router = express.Router()
+
 //router will go here
+router.get('/', (req, res) => {
+
+})
+
+router.post('/api/burgers', (req, res) => {
+
+})
+
+router.put('/api/burgers/:id', (req, res) => {
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+//dependencies
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+//set up express and ports
+const PORT = process.env.PORT || 7777;
+
+const app = express();
+
+//establish static server
+app.use(express.static('public'));
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//import routes and set up
+require('./controllers/burgers_controller.js')(app);
+
+app.listen(PORT, () => console.log(`App now listening on port ${PORT}`));
