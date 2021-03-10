f0ef027ee97fd920f7145551eada17072f402b35
Matt Autrey <mautrey17@gmail.com>
2021-03-10 00:06:59 -0500

    format burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,68 +1,58 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (event) {
-      console.info('DOM loaded');
-    }
+//wait for document to load before executing
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (event) {
+    console.info("DOM loaded");
+  }
 
-//create burger function
-const newBurgerForm = document.querySelector('#new-burger-form');
+  //create burger function
+  const newBurgerForm = document.querySelector("#new-burger-form");
 
-if(newBurgerForm) {
-    newBurgerForm.addEventListener('submit', (event) => {
-        event.preventDefault();
+  if (newBurgerForm) {
+    newBurgerForm.addEventListener("submit", (event) => {
+      event.preventDefault();
 
-        console.log('hi round 2');
+      //variable for new burger
+      const newBurger = {
+        burger_name: document.querySelector("#burger-name").value.trim(),
+        devoured: 0,
+      };
 
-        const newBurger = {
-            burger_name: document.querySelector('#burger-name').value.trim(),
-            devoured: 0,
+      fetch("/api/burgers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newBurger),
+      });
+      console.log("New burger successfully added");
+      location.reload();
+    });
+  }
+
+  //devour the burger (update MySQL and change burger to other list)
+  const devourerBtns = document.querySelectorAll(".eat-btn");
+
+  if (devourerBtns) {
+    console.log("I exist");
+    devourerBtns.forEach((btn) => {
+      btn.addEventListener("click", (event) => {
+        const id = event.target.getAttribute("data-id");
+        const nowEaten = {
+          devoured: 1,
         };
 
-        fetch('/api/burgers', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-            body: JSON.stringify(newBurger)
-        })
-        console.log("New burger successfully added")
-        location.reload()
-    })
-}
-
-//devour burger
-const devourerBtns = document.querySelectorAll('.eat-btn');
-
-
-if(devourerBtns){
-    console.log('I exist')
-    devourerBtns.forEach((btn) => {
-        btn.addEventListener('click', (event) => {
-            const id = event.target.getAttribute('data-id');
-            const nowEaten = {
-                devoured: 1,
-            };
-
-            fetch(`/api/burgers/${id}`, {
-                method: 'PUT', 
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify(nowEaten)
-            }).then(() => {
-                location.reload('/');
-            })
-    
-            // const newBurger = {
-            //     burger_name: document.querySelector('#burger-name').value.trim(),
-            //     devoured: document.querySelector('#uneaten').checked,
-            // };
-    
-            
-        })
-    })
-    
-}
-
+        fetch(`/api/burgers/${id}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(nowEaten),
+        }).then(() => {
+          location.reload("/");
+        });
+      });
+    });
+  }
 });
