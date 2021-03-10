89de2ea9c685f1297928dff818b36fc306b28064
Matt Autrey <mautrey17@gmail.com>
2021-03-10 16:21:42 -0500

    db connection fix

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,21 @@
 
 //dependencies
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '1234',
-  database: 'burgers_db',
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '1234',
+        database: 'burgers_db',
+      });
+}
+
 
 // establish connection
 connection.connect((err) => {
