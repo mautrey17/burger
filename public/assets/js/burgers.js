8cbb6e93fe1d4c899c44718c2e07c44b6fb36971
Matt Autrey <mautrey17@gmail.com>
2021-03-09 22:54:25 -0500

    ability to add new burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,16 +1,58 @@
-console.log('hi')
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+      console.info('DOM loaded');
+    }
 
-fetch('/api/burgers').then((resp) => {
-    return resp.json()
-}).then((data) => {
-    console.log(data)
-});
+// fetch('/api/burgers').then((resp) => {
+//     return resp.json()
+// }).then((data) => {
+//     console.log(data)
+// });
 
 //create burger function
-// const newBurgerForm = document.querySelector('#new-burger-form');
+const newBurgerForm = document.querySelector('#new-burger-form');
 
-// if(newBurgerForm) {
-//     newBurger.addEventListener('submit', (event) => {
-//         event.preventDefault();
-//     })
-// }
+if(newBurgerForm) {
+    newBurgerForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        console.log('hi round 2');
+
+        const newBurger = {
+            burger_name: document.querySelector('#burger-name').value.trim(),
+            devoured: 0,
+        };
+
+        fetch('/api/burgers', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+            body: JSON.stringify(newBurger)
+        })
+        console.log("New burger successfully added")
+        location.reload()
+    })
+}
+
+const test32 = document.getElementById('test');
+console.log(test32)
+
+if(test32){
+    console.log('I exist')
+    test32.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        console.log('hi round 2');
+
+        // const newBurger = {
+        //     burger_name: document.querySelector('#burger-name').value.trim(),
+        //     devoured: document.querySelector('#uneaten').checked,
+        // };
+
+        
+    })
+}
+
+});
